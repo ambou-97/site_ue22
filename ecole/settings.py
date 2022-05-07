94a94eb4c1fc6415034f8bd63b0e259092a34937
ambou-97 <ambroise.tine@gmail.com>
2022-05-07 02:43:18 +0000

    smtp2

--- ecole/settings.py
+++ ecole/settings.py
@@ -32,8 +32,9 @@ DEBUG = True
 #envoi email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'Suturasendmail@gmail.com'
+EMAIL_HOST_USER = 'suturasendmail@gmail.com'
 EMAIL_HOST_PASSWORD = 'suturaSendMail'
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
